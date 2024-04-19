bfce47bf580860e9406c28ba5e5021b71b84a174
joehefner <joehefner@outlook.com>
2024-04-19 13:27:33 -0500

    Project completion

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    #print(f"{num_words} words in {book_path}")
-    print(get_num_letters(text))
+    letters_count = get_num_letters(text)
+    letters_sort = sort_letters(letters_count)
+    for letter in letters_sort:
+        print(f"The '{letter['char']}' was found{letter['num']} times.")
 
 def get_num_words(text):
     words = text.split()
@@ -17,12 +19,20 @@ def get_num_letters(text):
     letters = {}
     lowercase_string = text.lower()
     for letter in lowercase_string:
-        if letter != " ":
+        if letter.isalpha():
             if letter in letters:
-                letters[letter] = letter
+                letters[letter] += 1
             else:
                 letters[letter] = 1
     return letters
+
+def sort_letters(dict):
+    letter_list = []
+    for entry in dict:
+        new_dict = {'char' : entry, "num" : dict[entry]}
+        letter_list.append(new_dict) 
+    letter_list.sort(reverse=True, key=sort_on)
+    return letter_list
 
 def sort_on(dict):
     return dict["num"]
